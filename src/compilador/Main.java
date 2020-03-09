9590b923063767821dbca7bdae57a0526a7ecd65
INGRID <ingridcarolinelima@hotmail.com>
2020-03-09 17:42:41 -0300

    teste arquivo

--- src/compilador/Main.java
+++ src/compilador/Main.java
@@ -1,8 +1,19 @@
 package compilador;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileReader codigo = new FileReader("codigo.txt");
+		BufferedReader file = new BufferedReader(codigo);
+		
+		while(file.ready()) {
+			String linha = file.readLine();
+			System.out.println(linha);
+		}
 		
 	}
 
