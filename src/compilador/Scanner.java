f5b4923ae8b7089b26f5f925b69f297c8a16db3f
INGRID <ingridcarolinelima@hotmail.com>
2020-03-12 11:09:39 -0300

    adicionado métodos de verificacao

--- src/compilador/Scanner.java
+++ src/compilador/Scanner.java
@@ -1,9 +1,41 @@
 package compilador;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
 	
-	private static char c = ' ';
-	public void proxToken() {
+	private List<String> bufferToke = new ArrayList<String>();
+	 
+	public List<String> proxToken(String c) {
 		
+		if(c.matches("[0-9]*")) {
+			bufferToke.add(c);
+			
+		}
+		
+		
+		return bufferToke;
+	}
+	
+	
+	public boolean verificaInteiro(List<String>token) {
+		for(String c: token) {
+			if(!c.matches("[0-9]*")) {
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	
+	private boolean verificaFloat() {
+		return false;
+	}
+	
+	
+	public void methodScanner(String caracter) {
+		List<String> token = proxToken(caracter);
+		verificaInteiro(token);
 	}
 }
