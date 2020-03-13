aa4d6927e7779127615d39e3a4f12a879498d225
INGRID <ingridcarolinelima@hotmail.com>
2020-03-13 14:44:57 -0300

    tokens

--- src/compilador/Token.java
+++ src/compilador/Token.java
@@ -1,16 +1,24 @@
 package compilador;
 
-public class Token {
+public enum Token {
 	
-	enum Dicionario {
-		PALAVRA_RESERVADA,
-		IDENTIFICADOR,
-		TIPO_INTEIRO,
-		TIPO_FLOAT,
-		OPERADOR_ATRIBUICAO,
-		OPERADOR_SOMA,
-		OPERADOR_MULTIPLOCACAO,
-		PONTO_E_VIRGULA,
-		
+	TIPO_INT(1),
+	TIPO_FLOAT(2),
+	TIPO_CHAR(3),
+	IDENTIFICADOR(4),
+	PONTO_E_VIRGULA(5),
+	PR_WHILE(5),
+	PR_IF(6),
+	PR_FOR(7),
+	PR_ELSE(8);
+	
+	private int id;
+	
+	Token(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
 	}
 }
