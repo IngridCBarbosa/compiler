d21a50459d7cd83a99dcc56b111a8564f7f14b00
INGRID <ingridcarolinelima@hotmail.com>
2020-03-13 13:13:34 -0300

    classe scanner

--- src/compilador/Scanner.java
+++ src/compilador/Scanner.java
@@ -1,41 +1,84 @@
 package compilador;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Scanner {
 	
-	private List<String> bufferToke = new ArrayList<String>();
-	 
-	public List<String> proxToken(String c) {
+	private boolean loop = true;
+	
+	private ReadeFile read = new ReadeFile();
+	private static char caracter = ' ';
+	
+	private List<String> buffer = new ArrayList<String>();
+
+	private int linha = 0;
+	private int coluna = 0;
+	
+	public void scannerToken() throws IOException {
 		
-		if(c.matches("[0-9]*")) {
-			bufferToke.add(c);
+		caracter = read.leituraCaracterArquivo();
+
+		while(loop) {
+			
+			if(Character.isDigit(caracter)) {
+				//buffer.add(String.valueOf(caracter)); // buffer ???
+				caracter = read.leituraCaracterArquivo();
+				
+				if(caracter == '.') {
+					caracter = read.leituraCaracterArquivo();
+					if(caracter == ';') {
+						mensagemErroFloat();
+					}
+					if(Character.isDigit(caracter)) {
+						caracter = read.leituraCaracterArquivo();
+						if(caracter == ';') {
+							
+						}
+					}
+				}
+				
+				
+			}
+			
+			if(caracter == '.') {
+				caracter = read.leituraCaracterArquivo();
+				if(Character.isDigit(caracter)) {
+					caracter = read.leituraCaracterArquivo();
+				}
+				
+					
+			}
+			
+			if(Character.isWhitespace(caracter)) {
+				//caracter = new Character(read.leituraCaracterArquivo());
+				
+				if(caracter == '\t') { // TAB
+					coluna = coluna + 4; 
+				}
+				
+				if(caracter == '\n') { // NEW LINE
+					linha++;
+				}
+				
+				else {
+					coluna++;
+				}
+				
+			}
+			
 			
 		}
-		
-		
-		return bufferToke;
 	}
 	
 	
-	public boolean verificaInteiro(List<String>token) {
-		for(String c: token) {
-			if(!c.matches("[0-9]*")) {
-				return false;
-			}
-		}
-		return false;
+	private void mensagemErroInt() {
+		System.err.println("int mal formado");
 	}
 	
-	
-	private boolean verificaFloat() {
-		return false;
+	private void mensagemErroFloat() {
+		System.err.println("fload mal formado");
 	}
 	
-	
-	public void methodScanner(String caracter) {
-		List<String> token = proxToken(caracter);
-		verificaInteiro(token);
-	}
 }
