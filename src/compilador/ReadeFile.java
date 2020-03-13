56294cc3bd946c96acfc24c14131530d851b1b1b
INGRID <ingridcarolinelima@hotmail.com>
2020-03-13 13:09:42 -0300

    classe de leitura de arquivo caracter a caracter

---
+++ src/compilador/ReadeFile.java
@@ -0,0 +1,31 @@
+package compilador;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadeFile {
+	private FileInputStream entrada;
+	private InputStreamReader entradaFormatada ; 
+	 
+	
+	public ReadeFile()  {
+		try {
+			entrada = new FileInputStream("file.txt");
+			entradaFormatada = new InputStreamReader(entrada);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} 
+	}
+
+
+	public char leituraCaracterArquivo() throws IOException {	
+		int c;
+	
+		c = entradaFormatada.read();
+		//System.out.println((char) c);
+		return (char) c;
+	}
+}
