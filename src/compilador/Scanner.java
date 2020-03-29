4fe60c61a21a11bcb65a54a7af771000c89418b9
INGRID <ingridcarolinelima@hotmail.com>
2020-03-29 18:58:02 -0300

    adicionado modificacaoes

--- src/compilador/Scanner.java
+++ src/compilador/Scanner.java
@@ -4,249 +4,236 @@ import java.io.IOException;
 
 public class Scanner {
 	
-	private boolean loop = true;
-	
 	private ReadeFile read = new ReadeFile();
 	private static char caracter = ' ';
 	
 	
-	private int linha = 1;
-	private int coluna = 1;
+	private int linha = 0;
+	private int coluna = 0;
 	
-	public Lexema scannerToken() throws IOException {
-		String forma_token;
+	public Token scannerToken() throws IOException {
+		
+		String forma_lexema = "";
+		
 		caracter = read.leituraCaracterArquivo();
-		forma_token = "" + caracter;
-		Lexema lexema = null;
-		while(loop) {
-			
-			// TIPO INTEIRO
-			if(Character.isDigit(caracter)) {
-				
+		
+		
+		while(Character.isWhitespace(caracter)) {
+			if(caracter == '\n' ) {
+				coluna = 0;
+				linha++;
+			}
+			if(caracter == '\t') {
+				coluna = coluna + 4;
+			}
+			else {
+				coluna++;
+			}
+			caracter = read.leituraCaracterArquivo();
+		}
+		
+		// NUMEROS INTEIROS
+		if(Character.isDigit(caracter)) {
+			while(Character.isDigit(caracter)) {
+				forma_lexema = forma_lexema + caracter;
 				caracter = read.leituraCaracterArquivo();
-				forma_token = forma_token + caracter;
-				
-				// TIPO FLOAT
+				// NUMERO FLOAT
 				if(caracter == '.') {
+					forma_lexema = forma_lexema + caracter;
 					caracter = read.leituraCaracterArquivo();
-					if(caracter == ';') {
-						lexema = new Lexema(forma_token,Token.TIPO_FLOAT_TOKEN);
-						mensagemErroFloat(linha ,coluna,lexema);
-						//return lexema;
-						break;
-					}
-					if(Character.isDigit(caracter)) {
-						forma_token = forma_token + caracter;
-						caracter = read.leituraCaracterArquivo();
-						if(caracter == ';') {
-							// float correto
-							lexema = new Lexema(forma_token,Token.TIPO_FLOAT_TOKEN);
-							//return lexema;
-							break;
-						}
-						if(caracter == ' ') {
-							coluna++;
-							// float correto 
-							lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
-							//return lexema;
-							break;
-						}
-					}
-					if(Character.isLetter(caracter)) {
-						lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
-						mensagemErroFloat(linha, coluna, lexema);
-						break;
-					}
-					
 					if(caracter == ' ') {
-						lexema = new Lexema(forma_token,Token.TIPO_FLOAT_TOKEN);
 						coluna++;
-						mensagemErroFloat(linha, coluna, lexema);
-						//return lexema;
+						mensagemDeErroFloat(linha, coluna);
 						break;
 					}
-					
-				
-					
+					while(Character.isDigit(caracter)) {
+						forma_lexema = forma_lexema + caracter;
+						caracter = read.leituraCaracterArquivo();
+					}
+					return new Token(forma_lexema, Dicionario.TIPO_FLOAT_TOKEN);
 					
 				}
-				if(caracter == ';') {
-					// int correto
-					lexema = new Lexema(forma_token,Token.TIPO_INT_TOKEN);
-					//return lexema;
-					break;
-				}else if(caracter == ' ') {
-					coluna++;
-					// int correto
-					lexema = new Lexema(forma_token,Token.TIPO_INT_TOKEN);
-					//return lexema;
-					break;
-				}
-				if(Character.isLetter(caracter)) {
-					forma_token = forma_token + caracter;
-					lexema = new Lexema(forma_token, Token.TIPO_INT_TOKEN);
-					mensagemErroInt(linha, coluna, lexema);
-					//return lexema;
-					break;
-				}
-				
 			}
-			
-			// TIPO FLOAT
-			if(caracter == '.') {
+			return new Token(forma_lexema, Dicionario.TIPO_INT_TOKEN);
+		}
+		
+		// NUMEROS FLOAT INICIADO POR PONTO
+		if(caracter == '.') {
+			forma_lexema = forma_lexema + caracter;
+			caracter = read.leituraCaracterArquivo();
+			if(caracter == ' ') {
+				mensagemDeErroFloat(linha, coluna);
+				return new Token(forma_lexema, Dicionario.ERRO);
+			}
+			while(Character.isDigit(caracter)) {
+				forma_lexema = forma_lexema + caracter;
 				caracter = read.leituraCaracterArquivo();
-				
-				if(Character.isDigit(caracter)) {
-					forma_token = forma_token + caracter;
+			}
+			return new Token(forma_lexema, Dicionario.TIPO_FLOAT_TOKEN);
+		}
+		
+		// TIPO CHAR
+		int aspasSimplesCodigo = 39;
+		char aspasSimples= (char) aspasSimplesCodigo;
+		
+		if(caracter == aspasSimples) {
+			forma_lexema = forma_lexema + caracter;
+			caracter = read.leituraCaracterArquivo();
+			while(Character.isLetterOrDigit(caracter)) {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+				if(caracter == aspasSimples) {
+					forma_lexema = forma_lexema + caracter;
+					return new Token(forma_lexema, Dicionario.TIPO_CHAR_TOKEN);
+				}
+				while(Character.isLetterOrDigit(caracter)) {
+					forma_lexema = forma_lexema + caracter;
 					caracter = read.leituraCaracterArquivo();
-					if(caracter == ';') {
-						// float correto
-						lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
+					if(caracter == aspasSimples) {
+						forma_lexema = forma_lexema + caracter;
+						mensagemDeErroChar(linha, coluna);
 						break;
 					}
-					if(caracter == ' ') {
-						lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
-						break;
-					}
-					
-					
 				}
 				
-				if(caracter == ';') {
-					lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
-					mensagemErroFloat(linha,coluna,lexema);
-					break;
-				}
-				if(caracter == ' ') {
-					lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
-					mensagemErroFloat(linha,coluna,lexema);
-					break;
-				}
-				if(Character.isWhitespace(caracter)) {
-					lexema = new Lexema(forma_token, Token.TIPO_FLOAT_TOKEN);
-					mensagemErroFloat(linha,coluna,lexema);
-					break;
-				}
-				
-					
 			}
-			
-			
-			
-			if(Character.isWhitespace(caracter)) {
-				
-				
-				if(caracter == '\t') { // TAB
-					coluna = coluna + 4; 
-				}
-				
-				if(caracter == '\n') { // NEW LINE
-					linha++;
-				}
-				
-				else {
-					coluna++;
-				}
+		}
+		
+		// CARACTERES ESPECIAIS
+		// ABRE PARENTESE
+		if(caracter == '(') {
+			while(caracter == '(') {
+				forma_lexema = forma_lexema + caracter;
 				caracter = read.leituraCaracterArquivo();
 			}
-			// FIM DE ARQUIVO
-			if(caracter == '|') {
-				 lexema = new Lexema ("-1",Token.FIM_DE_ARQUIVO_TOKEN);
-				 //return lexema;
-				 break;
+			return new Token(forma_lexema, Dicionario.ABRE_PARENTESE_TOKEN);
+		}
+		// FECHA PARENTESE
+		if(caracter == ')') {
+			while(caracter == ')') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.FECHA_PARENTESE_TOKEN);
+		}
+		// ABRE CHAVE
+		if(caracter == '{') {
+			while(caracter == '{') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.ABRE_CHAVE_TOKEN);
+		}
+		// FECHA CHAVES
+		if(caracter == '}') {
+			while(caracter == '}') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.FECHA_CHAVE_TOKEN);
+		}
+		
+		// PONTO E VÍRGULA
+		if(caracter == ';') {
+			while(caracter == ';') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.PONTO_E_VIRGULA_TOKEN);
+		}
+		
+		// VÍRGULA
+		if(caracter == ',') {
+			while(caracter == ',') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
 			}
 			
-			// TIPO CHAR
-			
-			 int aspasSimplesCodigo = 39;
-			 char aspasSimples = (char)aspasSimplesCodigo;
-			 
-			 if(caracter == aspasSimples) {
-				 caracter = read.leituraCaracterArquivo();
-				 if(Character.isLetterOrDigit(caracter)) {
-					 forma_token = forma_token + caracter;
-					 caracter = read.leituraCaracterArquivo();
-					 if(caracter == aspasSimples) {
-						 lexema = new Lexema(forma_token, Token.TIPO_CHAR_TOKEN);
-						 // return lexema;
-						 break;
-					 }
-					 else {
-						 lexema = new Lexema(forma_token,Token.TIPO_CHAR_TOKEN);
-						 mensagemErroChar(linha, coluna, lexema);
-						 break;
-					 }
-				 }
-				 if(Character.isWhitespace(caracter)) {
-					 forma_token = forma_token + caracter;
-					 caracter = read.leituraCaracterArquivo();
-					 if(caracter == aspasSimples) {
-						 lexema = new Lexema (forma_token, Token.TIPO_CHAR_TOKEN);
-						 //return lexema;
-						 break;
-					 }else {
-						 lexema = new Lexema(forma_token,Token.TIPO_CHAR_TOKEN);
-						 mensagemErroChar(linha, coluna, lexema);
-						 break;
-					 }
-				 }
-				 
-			 }
-			 
-			 // PALAVRAS RESERVADA OU IDENTIDICADOR
-			 if(Character.isLetter(caracter)) {
-				 caracter = read.leituraCaracterArquivo();
-				 if(Character.isLetter(caracter)) {
-					 forma_token = forma_token + caracter;
-					 caracter = read.leituraCaracterArquivo();
-					 if(Character.isWhitespace(caracter)) {
-						 
-					 }
-				 }
-			 }
-			 
-			 // CERACTER ESPECIAS
-			 if(caracter == '(') {
-				 lexema = new Lexema(forma_token, Token.ABRE_PARENTESE_TOKEN);
-				 break;
-			 }
-			 if(caracter == ')') {
-				 lexema = new Lexema(forma_token, Token.FECHA_PARENTESE_TOKEN);
-				 break;
-			 }
-			 if(caracter == ',') {
-				 lexema = new Lexema(forma_token, Token.VIRGULA_TOKEN);
-				 break;
-			 }
-			 if(caracter == ';') {
-				 lexema = new Lexema(forma_token, Token.PONTO_E_VIRGULA_TOKEN);
-				 break;
-			 }
-			
-			
-		}// FIM DO LOOP
+			return new Token(forma_lexema, Dicionario.VIRGULA_TOKEN);
+		}
 		
+		// OPERADOR RELACIONAL
 		
-		return lexema;
-	
+		if(caracter == '=') {
+			while(caracter == '=') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.OP_ARITMETICO_IGUAL_TOKEN);
+		}
+		
+		if(caracter == '+') {
+			while(caracter == '+') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.OP_ARITMETICO_ADICAO_TOKEN);
+		}
+		
+		if(caracter == '-') {
+			while(caracter == '-') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			
+			return new Token(forma_lexema, Dicionario.OP_ARITMETICO_SUBTRACAO_TOKEN);
+		}
+		
+		if(caracter == '*') {
+			while(caracter == '*') {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			return new Token(forma_lexema, Dicionario.OP_ARITMETICO_MULTIPLICACAO_TOKEN);
+		}
+		
+		// PALAVRA RESERVADA OU IDENTIFICADORES
+		if( caracter == '_' || Character.isLetter(caracter)) {
+			while(Character.isLetter(caracter) || caracter == '_' || Character.isDigit(caracter)) {
+				forma_lexema = forma_lexema + caracter;
+				caracter = read.leituraCaracterArquivo();
+			}
+			Token token = buscaPalavraReservada(forma_lexema);
+			return token;
+		}
+		if(caracter == '|') {
+			return new Token("-1", Dicionario.FIM_DE_ARQUIVO_TOKEN);
+		}
+		return new Token(forma_lexema, Dicionario.ERRO);
 	}
 	
-	
-	private void mensagemErroInt( int linha, int coluna,Lexema lexema) {
-		System.err.println("ERRO linha "+linha+", coluna "+coluna+" token: "+lexema.getToken()+" int mal formado");
+	private Token buscaPalavraReservada(String p) {
+		
+		switch (p) {
+		case "int":
+			return new Token(p, Dicionario.PR_INT_TOKEN);
+		case "if":
+			return new Token(p, Dicionario.PR_IF_TOKEN); 
+		case "else":
+			return new Token(p, Dicionario.PR_ELSE_TOKEN);
+		case "while":
+			return new Token(p, Dicionario.PR_WHILE_TOKEN);
+		case "do":
+			return new Token(p, Dicionario.PR_DO_TOKEN);
+		case "for":
+			return new Token(p,Dicionario.PR_FOR_TOKEN);
+		case "float":
+			return new Token(p,Dicionario.PR_FLOAT_TOKEN);
+		case "char":
+			return new Token(p, Dicionario.PR_CHAR_TOKEN);
+		case "main":
+			return new Token(p, Dicionario.PR_MAIN_TOKEN);
+		default: return new Token(p, Dicionario.IDENTIFICADOR_TOKEN);
+		}
+		
 	}
 	
-	
-	
-	private void mensagemErroFloat(int linha, int coluna,Lexema lexema) {
-		System.err.println("ERRO linha "+linha+", coluna "+coluna+" token: "+lexema.getToken()+" float mal formado");
+	private void mensagemDeErroFloat(int linha, int coluna) {
+		System.out.println("ERRO na linha "+linha+", coluna "+coluna+". Float mal formado");
 	}
 	
-	private void mensagemErroChar(int linha,int coluna,Lexema lexema) {
-		System.err.println("ERRO linha "+linha+", coluna "+coluna+" token: "+lexema.getToken()+" char mal formado");
+	private void mensagemDeErroChar(int linha, int coluna) {
+		System.out.println("ERRO na linha "+linha+", coluna "+coluna+". Char mal formado");
 	}
-	
 
-	
-	
 }
