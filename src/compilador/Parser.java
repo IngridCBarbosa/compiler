5e7bf18fdbfc6edd774f2a182bbf009620336c89
INGRID <ingridcarolinelima@hotmail.com>
2020-04-19 15:13:21 -0300

    adicionado erros no procedimento programa e bloco

--- src/compilador/Parser.java
+++ src/compilador/Parser.java
@@ -8,47 +8,54 @@ public class Parser {
 	private Scanner scanner;
 	private Token nextToken;
 	
-	public Parser(String nomeArquivo) {
+	public Parser(String nomeArquivo) throws IOException {
 		scanner = new Scanner(nomeArquivo);
+		nextToken = scanner.scannerToken();
 	}
-	
+	  
 	// VALIDA O MAIN
 	public void programa() throws IOException {
-		nextToken = scanner.scannerToken();
-		if(nextToken.getToken() != Dicionario.TIPO_INT_TOKEN) {
-			// ERRO
+		
+		if(nextToken.getToken() != Dicionario.PR_INT_TOKEN) {
+			mensagemErroInteiroInicioPrograma(Scanner.getLinha(), Scanner.getColuna());
+			System.exit(0);
 		}
 		nextToken = scanner.scannerToken();
 		if(nextToken.getToken() != Dicionario.PR_MAIN_TOKEN) {
-			// ERRO
+			mensagemErroMainInicioPrograma(Scanner.getLinha(), Scanner.getColuna());
+			System.exit(0);
 		}
 		nextToken = scanner.scannerToken();
 		if(nextToken.getToken() != Dicionario.ABRE_PARENTESE_TOKEN) {
-			// ERRO
+			mensagemAbreParentese(Scanner.getLinha(), Scanner.getColuna());
+			System.exit(0);
 		}
 		nextToken = scanner.scannerToken();
 		if(nextToken.getToken() != Dicionario.FECHA_PARENTESE_TOKEN) {
-			//ERRO
+			mensagemErroFechaParentese(Scanner.getLinha(), Scanner.getColuna());
+			System.exit(0);
 		}
-		// CHAMA O BLOCO
 		nextToken = scanner.scannerToken();
-		bloco();
 		
+		bloco();
+		System.out.println("PASSOU");
 	}
 	
 	private void bloco() throws IOException {
 		
 		
 		if(nextToken.getToken() != Dicionario.ABRE_CHAVE_TOKEN) {
-			
+			mensagemErroAbreChave(Scanner.getLinha(),Scanner.getColuna());
+			System.exit(0);
 		}
-		
 		nextToken = scanner.scannerToken();
+		
 		declaracaoVariavel();
 		
+		//comando();
 		
 		if(nextToken.getToken() != Dicionario.FECHA_CHAVE_TOKEN) {
-			System.out.println("ERRO }");
+			mensagemErroFechaChave(Scanner.getLinha(), Scanner.getColuna());
 			System.exit(0);
 		}
 		nextToken = scanner.scannerToken();
@@ -57,13 +64,13 @@ public class Parser {
 	
 	private void declaracaoVariavel() throws IOException {
 		
+		
 		while(nextToken.getToken() == Dicionario.PR_CHAR_TOKEN || nextToken.getToken() == Dicionario.PR_FLOAT_TOKEN || nextToken.getToken() == Dicionario.PR_INT_TOKEN ){
 			
-			tipo();
-		
+			nextToken = scanner.scannerToken();
+			
 			if(nextToken.getToken() != Dicionario.IDENTIFICADOR_TOKEN) {
-				// ERRO
-				System.out.println("ERRO sem identificador");
+				mensagemErroSemIdentificador(Scanner.getLinha(), Scanner.getColuna());
 				System.exit(0);
 			}
 		
@@ -71,40 +78,47 @@ public class Parser {
 			while(nextToken.getToken() == Dicionario.VIRGULA_TOKEN) {
 				nextToken = scanner.scannerToken();
 				if(nextToken.getToken() != Dicionario.IDENTIFICADOR_TOKEN) {
-					// ERRO
-					System.out.println("ERRO sem identificador");
+					mensagemErroSemIdentificador(Scanner.getLinha(), Scanner.getColuna());
 					System.exit(0);
 				}
 				nextToken = scanner.scannerToken();
 			}
 		
 			if(nextToken.getToken() != Dicionario.PONTO_E_VIRGULA_TOKEN) {
-				// ERRO
-				System.out.println("ERRO sem ;");
+				mensagemErroPontoEVirgula(Scanner.getLinha(), Scanner.getColuna());
 				System.exit(0);
 			}
 			nextToken = scanner.scannerToken();
-			
 		}
-	}
-	
-	
-	private void tipo() throws IOException {
-		//nextToken = scanner.scannerToken();
-		if(nextToken.getToken() != Dicionario.PR_INT_TOKEN) {
-			// ERRO
-		}
-		if(nextToken.getToken() != Dicionario.PR_FLOAT_TOKEN) {
-			// ERRO
-		}
-		if(nextToken.getToken() != Dicionario.PR_CHAR_TOKEN) {
-			// ERRO
-		}
-		nextToken = scanner.scannerToken();
-	}
-	
-	private void comando() {
 		
 	}
 	
+	
+	
+	
+	/// ---------------------------ERROS----------------------------////
+	private void mensagemErroInteiroInicioPrograma(int linha, int coluna) {
+		System.out.println("Erro na linha "+linha+", coluna "+coluna+" Iniciado programa sem INT");
+	}
+	private void mensagemErroMainInicioPrograma(int linha, int coluna) {
+		System.out.println("Erro na linha "+linha+", coluna "+coluna+". Iniciado programa sem MAIN");
+	}
+	private void mensagemAbreParentese(int linha, int coluna) {
+		System.out.println("Erro na linha "+linha+" coluna "+coluna+". Sem o (");
+	}
+	private void mensagemErroFechaParentese(int linha, int coluna) {
+		System.out.println("Erro na linha "+linha+" coluna "+coluna+". Sem o )");
+	}
+	private void mensagemErroAbreChave(int linha, int coluna) {
+		System.out.println("Erro na linha "+linha+"coluna "+coluna+". Sem o {");
+	}
+	private void mensagemErroFechaChave(int linha, int coluna) {
+		System.out.println("Erro na linha "+linha+"coluna "+coluna+". Sem o }");
+	}
+	private void mensagemErroSemIdentificador(int linha, int coluna) {
+		System.out.println("Erro na linha"+linha+", coluna "+coluna+". Sem o identidicador");
+	}
+	private void mensagemErroPontoEVirgula(int linha, int coluna) {
+		System.out.println("Erro na linha"+linha+", coluna"+coluna+". Sem o ;");
+	}
 }
